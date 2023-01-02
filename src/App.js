40ea1ea0a23f12500600193ae58055680e71524b
Cupcakes33 <rute930627@gmail.com>
2023-01-02 21:57:25 +0900

    :sparkles:	feat: 카카오, 네이버 소셜로그인 구현. 백엔드와 상의 필요

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-
+import KakaoLogin from "./components/socialLogin/KakaoLogin";
+import NaverLogin from "./components/socialLogin/NaverLogin";
 function App() {
   return (
-    <div className="App">
-      
+    <div
+      className="App"
+      style={{ display: "flex", flexDirection: "column", gap: "30px" }}
+    >
+      <div>
+        <KakaoLogin />
+      </div>
+      <div>
+        <NaverLogin />
+      </div>
     </div>
   );
 }
