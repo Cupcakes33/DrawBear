6ea53f2db1b96d52baa0bc3bd8dab8c20d08dfb0
jhchoi1182 <jhchoi1182@gmail.com>
2023-01-03 09:25:08 +0900

    :sparkles: feat: 로그인 유효성에 따른 css 적용 Resolves: #4

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,27 +9,33 @@ const Login = () => {
     formState: { errors },
   } = useForm();
 
-  console.log(errors);
-
   const onSubmit = (data) => console.log(data); // console.log 대신 디스패치 들어가면 됨
 
   return (
     <TestForm onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="email">이메일</label>
-        <input type="email" id="email" name="email" {...register("email", { required: true })} aria-invalid={errors.email ? "true" : "false"}></input>
-        {errors?.email && <span role="alert">⚠이메일을 적어주세요.</span>}
+        <input
+          className={errors?.email?.type === undefined ? "pass" : "fail"}
+          type="email"
+          id="email"
+          name="email"
+          {...register("email", { required: true, pattern: /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/ })}
+          aria-invalid={errors.email ? "true" : "false"}
+        ></input>
+        {errors?.email?.type && <span role="alert">⚠이메일 형식에 맞지 않습니다.</span>}
       </div>
       <div>
         <label htmlFor="password">비밀번호</label>
         <input
+          className={errors?.password?.type === undefined ? "pass" : "fail"}
           type="password"
           id="password"
           name="password"
-          {...register("password", { required: true, minLength: 3, maxLength: 10 })}
+          {...register("password", { required: true, pattern: /^[a-z0-9_-]{4,16}$/ })}
           aria-invalid={errors?.password ? "true" : "false"}
         ></input>
-        {errors?.password && <span role="alert">⚠3~10자 이내로 적어주세요</span>}
+        {errors?.password && <span role="alert">⚠4~16자 이내로 적어주세요</span>}
       </div>
       <div>
         <button>로그인</button>
@@ -43,9 +49,17 @@ const TestForm = styled.form`
   span {
     color: red;
   }
-  input:focus {
-    /* border-color: red;
-    outline: none; */
+  .pass:focus {
+    transition: box-shadow 0.4s;
+    border: 1px solid green;
+    box-shadow: 0 0 5px green;
+    outline: none;
+  }
+  .fail:focus {
+    transition: box-shadow 0.4s;
+    border: 1px solid red;
+    box-shadow: 0 0 5px red;
+    outline: none;
   }
 `;
 
