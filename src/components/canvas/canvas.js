55541bdb448cbf6e83b503c69a2b29c8b5b39146
Cupcakes33 <rute930627@gmail.com>
2023-01-03 19:51:53 +0900

    :sparkles:	feat: 캔버스 구성요소 추가작업 및 이벤트 작명

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -1,3 +1,38 @@
-const Canvas = () => {};
+import { useState, useEffect } from "react";
+import rough from "roughjs/bundled/rough.esm";
+
+const generator = rough.generator();
+
+const Canvas = () => {
+  const [elements, setElements] = useState([]);
+  const [drawing, setDrawing] = useState(false);
+
+  const handleMouseDown = (event) => {};
+  const handleMouseMove = (evnet) => {};
+  const handleMouseUp = (event) => {};
+
+  useEffect(() => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    const roughCanvas = rough.canvas(canvas);
+    const rect = generator.rectangle(100, 100, 100, 100);
+    const line = generator.line(100, 100, 200, 200);
+    roughCanvas.draw(rect);
+    roughCanvas.draw(line);
+  });
+
+  return (
+    <canvas
+      id="canvas"
+      styled={{ backgroundColor: "blue" }}
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    />
+  );
+};
 
 export default Canvas;
--- src/shares/Router.js
+++ src/shares/Router.js
@@ -1,14 +1,14 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "../pages/Signup";
-import Login from '../pages/Login'
-
+import Login from "../pages/Login";
+import Canvas from "../components/canvas/Canvas";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-              <Route path='/' element={<Login />} />
+        <Route path="/canvas" element={<Canvas />} />
+        <Route path="/" element={<Login />} />
         <Route path="/" element={<Signup />} />
       </Routes>
     </BrowserRouter>
