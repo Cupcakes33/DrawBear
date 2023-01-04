bd619256ff422c4aa3328728568e252e5cd9ec4f
jhchoi1182 <jhchoi1182@gmail.com>
2023-01-04 19:32:18 +0900

    :lipstick: feat: 태그에 스타일드 컴포넌트 적용

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,9 +1,11 @@
+import styled from "styled-components"
+
 const Calendar = () => {
   return (
-    <section>
-      <div>
+    <Container>
+      <StHeader>
         <h3>2023년 1월</h3>
-        <div>
+        <div className="buttons">
           <div>
             <button>이전 달</button>
             <button>다음 달</button>
@@ -12,11 +14,41 @@ const Calendar = () => {
             <button>x</button>
           </div>
         </div>
-      </div>
-      <div className="week">주</div>
-      <div className="date">일</div>
-    </section>
+      </StHeader>
+      <StWeek>주</StWeek>
+      <StDate>일</StDate>
+    </Container>
   )
 }
+
+const Container = styled.section`
+  width: 500px;
+  height: 400px;
+  padding: 20px 20px;
+  border: 1px solid black;
+`
+
+const StHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  .buttons {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+`
+
+const StWeek = styled.div`
+  width: calc(500px / 7);
+  text-align: center;
+`
+
+const StDate = styled.div`
+  float: left;
+  width: calc(500px / 7);
+  height: 50px;
+  margin-top: 20px;
+  text-align: center;
+`
 
 export default Calendar
