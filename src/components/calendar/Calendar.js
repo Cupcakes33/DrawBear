9f5f27ae0c05ca3f367a840bdfc388f6d862a2b4
jhchoi1182 <jhchoi1182@gmail.com>
2023-01-04 19:12:49 +0900

    :sparkles: feat: 달력을 구성하는 태그들 작성

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,6 +1,21 @@
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <section>
+      <div>
+        <h3>2023년 1월</h3>
+        <div>
+          <div>
+            <button>이전 달</button>
+            <button>다음 달</button>
+          </div>
+          <div>
+            <button>x</button>
+          </div>
+        </div>
+      </div>
+      <div className="week">주</div>
+      <div className="date">일</div>
+    </section>
   )
 }
 
