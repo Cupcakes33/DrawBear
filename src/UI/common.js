295504d2da3e3688d72433026c48f58a210407ef
Cupcakes33 <rute930627@gmail.com>
2023-01-11 23:14:02 +0900

    :lipstick: FIX: common ui components flex props 옵션 추가

--- src/UI/common.js
+++ src/UI/common.js
@@ -1,11 +1,25 @@
 import styled, { css } from "styled-components";
 
+const flexProps = css`
+  ${(props) =>
+    props.flex &&
+    css`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    `}
+  flex-direction: ${(props) => props.derection};
+  justify-content: ${(props) => props.justify};
+  align-items: ${(props) => props.align};
+`;
+
 export const StContainer = styled.div`
   position: relative;
   width: 360px;
   height: 100vh;
   border: 1px solid black;
   background-color: white;
+  ${flexProps}
 `;
 
 export const StHeader = styled.header`
@@ -13,13 +27,8 @@ export const StHeader = styled.header`
   height: 60px;
   background-color: #f5f5f5;
   position: relative;
-  ${(props) =>
-    props.flexCenter &&
-    css`
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    `}
+  padding: 20px;
+  ${flexProps}
 `;
 
 export const StSection = styled.section`
@@ -29,6 +38,7 @@ export const StSection = styled.section`
   position: relative;
   overflow-x: scroll;
   padding: 20px;
+  ${flexProps}
 `;
 
 export const StFooter = styled.div`
