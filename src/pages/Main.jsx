e596bbdb23a95528c9707725469372befae3c7e4
Cupcakes33 <rute930627@gmail.com>
2023-01-11 09:26:36 +0900

    :lipstick: FIX: common 파일 위치 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
-import styled from "styled-components";
 import DiaryList from "../components/main/DiaryList";
 import NoDiary from "../components/main/NoDiary";
 import Footer from "../components/common/Footer";
-import { StContainer, StHeader, StSection } from "../components/common/common";
+import { StContainer, StHeader, StSection } from "../UI/common";
 
 const Main = () => {
   const [isDiaryData, setIsDiaryData] = useState(true);
   return (
     <StContainer>
-      <DetailHeader flexCenter>
+      <StHeader flexCenter>
         <h1>LOGO</h1>
-      </DetailHeader>
+      </StHeader>
       <StSection>{!isDiaryData ? <NoDiary /> : <DiaryList />}</StSection>
       <Footer></Footer>
     </StContainer>
@@ -19,33 +18,3 @@ const Main = () => {
 };
 
 export default Main;
-
-// const StContainer = styled.div`
-//   width: 360px;
-//   height: 100vh;
-//   border: 1px solid black;
-//   background-color: white;
-//   position: relative;
-// `;
-
-const DetailHeader = styled(StHeader)`
-  h1 {
-    color: red;
-  }
-`;
-
-// const StHeader = styled.header`
-//   width: 100%;
-//   height: 60px;
-//   background-color: #f5f5f5;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `;
-
-// const StSection = styled.section`
-//   width: 100%;
-//   height: calc(100% - 132px);
-//   background-color: white;
-//   overflow-x: scroll;
-// `;
