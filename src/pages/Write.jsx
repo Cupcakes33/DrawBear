4904700c99bff004c29f665552b306b4ee1df697
Cupcakes33 <rute930627@gmail.com>
2023-01-11 16:54:15 +0900

    :lipstick: ADD: 제목 페이지 css 작업

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -22,17 +22,18 @@ const Write = () => {
       </StCanvasSection>
       <StTitleSection drawing={isDrawing}>
         <div>
-          <span>제목 : </span>
+          <span>제목 :</span>
           <input type="text" placeholder="제목을 입력해주세요" />
         </div>
         <div>
-          <span>날짜 : </span>
+          <span>날짜 :</span>
           <input type="date" placeholder="2023.01.01" />
         </div>
         <div>
-          <span>태그 : </span>
+          <span>태그 :</span>
           <input type="text" placeholder="태그를 입력해주세요" />
         </div>
+        <button>일기 작성하기</button>
       </StTitleSection>
     </StContainer>
   );
@@ -67,7 +68,6 @@ const StTitleSection = styled(StSection)`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   gap: 20px;
   padding: 10px;
   ${(props) =>
@@ -75,4 +75,26 @@ const StTitleSection = styled(StSection)`
     css`
       display: none;
     `}
+  div {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+  }
+  input {
+    width: 80%;
+    height: 30px;
+    border: 1px solid #d9d9d9;
+    border-radius: 5px;
+    padding: 10px;
+  }
+  button {
+    width: 100%;
+    height: 30px;
+    border: 1px solid #d9d9d9;
+    border-radius: 5px;
+    cursor: pointer;
+  }
 `;
