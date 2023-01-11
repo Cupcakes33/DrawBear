bc9ec8435499f77ab6179cf52dcd894b698d9c38
Cupcakes33 <rute930627@gmail.com>
2023-01-11 09:11:55 +0900

    :lipstick: ADD: 공통으로 쓰는 styled components 추가

---
+++ src/components/common/common.js
@@ -0,0 +1,41 @@
+import styled, { css } from "styled-components";
+
+export const StContainer = styled.div`
+  position: relative;
+  width: 360px;
+  height: 100vh;
+  border: 1px solid black;
+  background-color: white;
+`;
+
+export const StHeader = styled.header`
+  width: 100%;
+  height: 60px;
+  background-color: #f5f5f5;
+  ${(props) =>
+    props.flexCenter &&
+    css`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    `}
+`;
+
+export const StSection = styled.section`
+  width: 100%;
+  height: calc(100% - 132px);
+  background-color: white;
+  overflow-x: scroll;
+`;
+
+export const StFooter = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 72px;
+  background-color: #f8f8f8;
+`;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,14 +3,15 @@ import styled from "styled-components";
 import DiaryList from "../components/main/DiaryList";
 import NoDiary from "../components/main/NoDiary";
 import Footer from "../components/common/Footer";
+import { StContainer, StHeader, StSection } from "../components/common/common";
 
 const Main = () => {
   const [isDiaryData, setIsDiaryData] = useState(true);
   return (
     <StContainer>
-      <StHeader>
+      <DetailHeader flexCenter>
         <h1>LOGO</h1>
-      </StHeader>
+      </DetailHeader>
       <StSection>{!isDiaryData ? <NoDiary /> : <DiaryList />}</StSection>
       <Footer></Footer>
     </StContainer>
@@ -19,26 +20,32 @@ const Main = () => {
 
 export default Main;
 
-const StContainer = styled.div`
-  width: 360px;
-  height: 100vh;
-  border: 1px solid black;
-  background-color: white;
-  position: relative;
+// const StContainer = styled.div`
+//   width: 360px;
+//   height: 100vh;
+//   border: 1px solid black;
+//   background-color: white;
+//   position: relative;
+// `;
+
+const DetailHeader = styled(StHeader)`
+  h1 {
+    color: red;
+  }
 `;
 
-const StHeader = styled.header`
-  width: 100%;
-  height: 60px;
-  background-color: #f5f5f5;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+// const StHeader = styled.header`
+//   width: 100%;
+//   height: 60px;
+//   background-color: #f5f5f5;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
 
-const StSection = styled.section`
-  width: 100%;
-  height: calc(100% - 132px);
-  background-color: white;
-  overflow-x: scroll;
-`;
+// const StSection = styled.section`
+//   width: 100%;
+//   height: calc(100% - 132px);
+//   background-color: white;
+//   overflow-x: scroll;
+// `;
