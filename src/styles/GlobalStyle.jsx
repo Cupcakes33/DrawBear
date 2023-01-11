fe77dad6546f3b9da309e2e733a7a58954a9499e
jhchoi1182 <jhchoi1182@gmail.com>
2023-01-11 23:42:40 +0900

    💄 ADD: 스크롤 숨기기 보류

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -8,6 +8,11 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-size: 1.6rem;
+    /* -ms-overflow-style: none;
+    scrollbar-width: none;
+    ::-webkit-scrollbar {
+      display:none;
+    } */
   }
   a {
     text-decoration:none;
