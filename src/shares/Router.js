fc9450f7ed26f20c12a9220b824574f28787b65b
Cupcakes33 <rute930627@gmail.com>
2023-01-11 10:20:28 +0900

    :fire: DELETE: 기존 캔버스 삭제

--- src/shares/Router.js
+++ src/shares/Router.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
 import Write from "../pages/Write";
-import FabricCanvas from "../components/FabricCanvas/FabricCanvas";
+import FabricCanvas from "../components/FabricCanvas/Canvas";
 import Main from "../pages/Main";
 import CreateDiary from "../pages/CreateDiary";
 import FullList from "../pages/FullList";
