bace813e1e155e55c46c484d3c776b9c4a742337
jhchoi1182 <jhchoi1182@gmail.com>
2023-01-12 17:40:06 +0900

    :fire:FIX: 기술 스택 변경으로 인한 로그인 thunk 삭제

--- src/redux/modules/loginSlice.js
+++ src/redux/modules/loginSlice.js
@@ -1,24 +1,9 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import { loginApi } from "../../apis/axios";
 
 const initialState = {
   holiday: []
 };
-
-export const __login = createAsyncThunk(
-  "LOGIN_FINALE",
-  async (payload, thunkAPI) => {
-    try {
-      const { data } = await loginApi.login(payload)
-      localStorage.setItem("token", data.token);
-
-      return thunkAPI.fulfillWithValue(data)
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message)
-    }
-  }
-)
 
 export const __holiday = createAsyncThunk(
   "HOLIDAY_FINALE",
@@ -38,17 +23,6 @@ const loginSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(__login.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(__login.fulfilled, (state, action) => {
-        state.isLoading = false;
-      })
-      .addCase(__login.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      })
-
       .addCase(__holiday.pending, (state) => {
         state.isLoading = true;
       })
