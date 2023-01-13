f508632ea0ec7a93476a0a3ff34419adf5ae010a
jhchoi1182 <jhchoi1182@gmail.com>
2023-01-13 09:49:25 +0900

    ✨ADD: 메인 페이지 에러 핸들링 작성

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import DiaryList from "../components/main/DiaryList";
 import NoDiary from "../components/main/NoDiary";
 import Footer from "../components/common/Footer";
@@ -13,25 +13,38 @@ const Main = () => {
   const [isDiaryData, setIsDiaryData] = useState(false);
   const dispatch = useDispatch();
 
-
   const { data, isError, isLoading, error } = useQuery(["main"], mainApi.read);
-  
+
   // useEffect(() => {
   //   // dispatch(__main());
-    // axios.get(`${process.env.REACT_APP_MY_API}/api/diary`).then((response) => {
-    //   console.log(response);
-    // });
+  // axios.get(`${process.env.REACT_APP_MY_API}/api/diary`).then((response) => {
+  //   console.log(response);
+  // });
   // }, []);
-  console.log(data);
+
+  const errorHandler = useCallback(() => {
+    const { status } = error.response.request;
+    if (status === 401) return <h2>로그인 후 이용 가능한 기능입니다.</h2>;
+    else if (status === 404) return <h2>일기장이 존재하지 않습니다.</h2>;
+    else return <h2>일기장 조회에 실패했습니다.</h2>;
+  }, [error]);
 
   return (
-    <StContainer>
-      <StHeader flexCenter>
-        <h1>LOGO</h1>
-      </StHeader>
-      <StSection>{!isDiaryData ? <NoDiary /> : <DiaryList />}</StSection>
-      <Footer></Footer>
-    </StContainer>
+    <>
+      {isLoading ? (
+        <h2>로딩 중...</h2>
+      ) : isError ? (
+        errorHandler()
+      ) : (
+        <StContainer>
+          <StHeader flexCenter>
+            <h1>LOGO</h1>
+          </StHeader>
+          <StSection>{!isDiaryData ? <NoDiary /> : <DiaryList />}</StSection>
+          <Footer></Footer>
+        </StContainer>
+      )}
+    </>
   );
 };
 
