a57d694416e0dc53685bf763ea3630441a694d59
hyemin0303 <hyeminpak@naver.com>
2023-01-20 05:04:19 +0900

    :sparkles: ADD: 회원정보 불러오기 기능구현완료

--- src/pages/Setting/AccoutDelete.jsx
+++ src/pages/Setting/AccoutDelete.jsx
@@ -3,15 +3,21 @@ import styled from "styled-components";
 import NavigateBtn from "../../components/common/NavigateBtn";
 import AccountDeleteBear from "../../assets/images/account_delete_bear.webp";
 import Button from "../../components/common/Button";
-
-const myProfileData = {
-  id: 1,
-  name: "김철수",
-  email: "abc@naver.com",
-  profile: "https://cdn-icons-png.flaticon.com/512/5312/5312933.png",
-};
+import { useQuery } from "@tanstack/react-query";
+import { mypageApi } from "../../apis/axios";
+import { useEffect, useState } from "react";
 
 const AccoutDelete = () => {
+  const { data } = useQuery(["myProfileData"], mypageApi.read);
+
+  const [userinfo, setUserInfo] = useState({
+    nickName: "",
+  });
+  useEffect(() => {
+    setUserInfo({
+      nickName: data?.userInfo.nickname,
+    });
+  }, []);
   return (
     <StContainer>
       <StHeader flex justify="flex-start">
@@ -23,9 +29,12 @@ const AccoutDelete = () => {
           <img src={AccountDeleteBear} alt="탈퇴곰돌이" />
         </div>
         <div className="warning">
-          <h2>{myProfileData.name}님</h2>
+          <h2>{userinfo.nickName}님</h2>
           <h2>정말 떠나시겠어요? {": ("}</h2>
-          <h4>내 프로필 사진, 댓글, 다이어리, 내용 등 모든 활동 정보가 삭제되며, 삭제된 데이터는 복구할 수 없어요.</h4>
+          <h4>
+            내 프로필 사진, 댓글, 다이어리, 내용 등 모든 활동 정보가 삭제되며,
+            삭제된 데이터는 복구할 수 없어요.
+          </h4>
         </div>
         <div className="delete-button">
           <Button fullWidth color="button_alart">
