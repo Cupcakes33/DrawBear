dd32c1d8fc4acf7e877a5c90ba6d499cb77b8f9a
hyemin0303 <hyeminpak@naver.com>
2023-01-20 05:34:59 +0900

    :art: FIX: 회원탈퇴 비밀번호 입력란 view 완료

--- src/pages/Setting/AccoutDelete.jsx
+++ src/pages/Setting/AccoutDelete.jsx
@@ -3,21 +3,29 @@ import styled from "styled-components";
 import NavigateBtn from "../../components/common/NavigateBtn";
 import AccountDeleteBear from "../../assets/images/account_delete_bear.webp";
 import Button from "../../components/common/Button";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { mypageApi } from "../../apis/axios";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const AccoutDelete = () => {
-  const { data } = useQuery(["myProfileData"], mypageApi.read);
-
+  const { data, isLoading } = useQuery(["myProfileData"], mypageApi.read);
   const [userinfo, setUserInfo] = useState({
     nickName: "",
   });
+  const deleteOnclickHandle = () => {
+    // const { data } = useMutation();
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, isDirty, errors },
+  } = useForm({ mode: "onchange" });
   useEffect(() => {
     setUserInfo({
       nickName: data?.userInfo.nickname,
     });
-  }, []);
+  }, [isLoading]);
   return (
     <StContainer>
       <StHeader flex justify="flex-start">
@@ -25,22 +33,42 @@ const AccoutDelete = () => {
         <h3>회원 탈퇴</h3>
       </StHeader>
       <StMypageSection flex derection="column" justify="flex-start">
-        <div className="myProfileInfoWrapper">
-          <img src={AccountDeleteBear} alt="탈퇴곰돌이" />
-        </div>
-        <div className="warning">
-          <h2>{userinfo.nickName}님</h2>
-          <h2>정말 떠나시겠어요? {": ("}</h2>
-          <h4>
-            내 프로필 사진, 댓글, 다이어리, 내용 등 모든 활동 정보가 삭제되며,
-            삭제된 데이터는 복구할 수 없어요.
-          </h4>
-        </div>
-        <div className="delete-button">
-          <Button fullWidth color="button_alart">
-            네, 탈퇴할래요.
-          </Button>
-        </div>
+        <form>
+          <div className="myProfileInfoWrapper">
+            <img src={AccountDeleteBear} alt="탈퇴곰돌이" />
+          </div>
+          <div className="warning">
+            <h2>{userinfo.nickName}님</h2>
+            <h2>정말 떠나시겠어요? {": ("}</h2>
+            <h4>
+              내 프로필 사진, 댓글, 다이어리, 내용 등 모든 활동 정보가 삭제되며,
+              삭제된 데이터는 복구할 수 없어요.
+            </h4>
+          </div>
+          <div className="delete-button" onClick={deleteOnclickHandle}>
+            <Button fullWidth color="button_alart">
+              네, 탈퇴할래요.
+            </Button>
+          </div>
+          <div>
+            <h4>회원탈퇴를 위해 비밀번호를 입력해주세요</h4>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="비밀번호를 입력해주세요"
+              aria-invalid={
+                !isDirty ? undefined : errors.password ? false : true
+              }
+              {...register("password", {
+                required: "비밀번호는 필수 입력 입니다.",
+              })}
+            />
+            {errors.password && (
+              <small role="alert">{errors.password.message}</small>
+            )}
+          </div>
+        </form>
       </StMypageSection>
     </StContainer>
   );
