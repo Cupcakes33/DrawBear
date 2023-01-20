4a4c5dea8c3c9b3c3c660b0a5524ef495e42d64d
jhchoi1182 <jhchoi1182@gmail.com>
2023-01-20 15:58:13 +0900

    🐛 FIX: 메인 페이지 useQuery 실패 시 무한 렌더링 발생하는 버그 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import DiaryList from "../components/main/DiaryList";
 import NoDiary from "../components/main/NoDiary";
 import Footer from "../components/common/Footer";
@@ -8,21 +8,29 @@ import { mainApi } from "../apis/axios";
 import { useDispatch, useSelector } from "react-redux";
 import { showModal } from "../redux/modules/UISlice";
 import Alert from "../components/common/modal/Alert";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
-  const dispatch = useDispatch();
   const { diaryTypes } = useSelector((state) => state.diarySlice);
   const { isModal } = useSelector((state) => state.UISlice);
+  const dispatch = useDispatch();
 
-  const { data = [], isError, isLoading, error } = useQuery(["main"], mainApi.read);
+  const {
+    data = [],
+    isError,
+    isLoading,
+    error,
+  } = useQuery(["main"], mainApi.read, {
+    onError: (error) => {
+      const { status } = error?.response.request;
+      if (status === 401) {
+        dispatch(showModal({ isModal: true, content: "로그인 후 이용해주세요.", move: "/login" }));
+      } else if (status === 400)
+        return dispatch(showModal({ isModal: true, content: "일기장 조회에 실패했습니다.", move: "/login" }));
+    },
+  });
   const { diaries } = data;
 
-  const errorHandler = useCallback(() => {
-    const { status } = error?.response.request;
-    if (status === 401) {
-      dispatch(showModal({ isModal: true, content: "로그인이 만료되었습니다.", move: "/login" }));
-    } else if (status === 400) return <h2>일기장 조회에 실패했습니다.</h2>;
-  }, [error]);
   const diaryType = useCallback(
     (diaries) => {
       if (diaryTypes.couple === 0) {
@@ -49,7 +57,7 @@ const Main = () => {
       {isLoading ? (
         <h2>로딩 중...</h2>
       ) : isError ? (
-        errorHandler()
+        <h2>{`${error?.response.status} ERROR`}</h2>
       ) : (
         <StContainer bgColor="#F8F8F8">
           <StHeader flex>
