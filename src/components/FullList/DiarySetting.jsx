367702e570cdb05b9d1afcdede6743b94f485aeb
jhchoi1182 <jhchoi1182@gmail.com>
2023-01-23 23:09:20 +0900

    ✨ ADD: 멤버 초대 페이지로 연결하는 로직 추가

--- src/components/FullList/DiarySetting.jsx
+++ src/components/FullList/DiarySetting.jsx
@@ -15,12 +15,8 @@ const DiarySetting = ({ onClose, queryClient, diaryId }) => {
   const { data, mutate } = useMutation((id) => mainApi.delete(id), {
     onError: (error) => {
       const status = error?.response.request.status;
-      if (status === 404)
-        dispatch(
-          showModal({ isModal: true, content: "다이어리가 존재하지 않습니다." })
-        );
-      else if (status === 401)
-        dispatch(showModal({ isModal: true, content: "권한이 없습니다." }));
+      if (status === 404) dispatch(showModal({ isModal: true, content: "다이어리가 존재하지 않습니다." }));
+      else if (status === 401) dispatch(showModal({ isModal: true, content: "권한이 없습니다." }));
       else if (status === 500)
         dispatch(
           showModal({
@@ -30,9 +26,7 @@ const DiarySetting = ({ onClose, queryClient, diaryId }) => {
         );
     },
     onSuccess: () => {
-      dispatch(
-        showModal({ isModal: true, content: "다이어리 삭제 성공!", move: "/" })
-      );
+      dispatch(showModal({ isModal: true, content: "다이어리 삭제 성공!", move: "/" }));
       queryClient?.invalidateQueries({ queryKey: ["main"] });
     },
   });
@@ -41,11 +35,9 @@ const DiarySetting = ({ onClose, queryClient, diaryId }) => {
     <>
       <Modal onClose={onClose} modalWidth="36rem" top="94%" radius="0">
         <DiarySettingModal>
-          <div>같이 쓰는 멤버 초대</div>
+          <div onClick={() => navigate("/invite")}>같이 쓰는 멤버 초대</div>
           <hr />
-          <div onClick={() => navigate(`/update/${diaryId}`)}>
-            다이어리 수정
-          </div>
+          <div onClick={() => navigate(`/update/${diaryId}`)}>다이어리 수정</div>
           <hr />
           <div onClick={() => mutate(diaryId)}>다이어리 삭제</div>
         </DiarySettingModal>
