0ae07eb62c5a7ef803549a447c8a3a2f552aa611
jhchoi1182 <jhchoi1182@gmail.com>
2023-01-28 03:14:35 +0900

    ✨ FIX: 모달 빠진 것 수정

--- src/components/main/NoDiary.jsx
+++ src/components/main/NoDiary.jsx
@@ -1,26 +1,22 @@
-import { useState } from "react";
 import styled from "styled-components";
 import CreateDiaryModal from "./CreateDiaryModal";
 import NoDiaryBear from "../../assets/images/noDiaryBear.webp";
+import { flex } from "../../UI/common";
 
 const NoDiary = () => {
-  const [isModal, setIsModal] = useState(false);
   return (
     <>
       <StContainer>
         <StWrapper>
           <h3>아직 다이어리가 없어요</h3>
-          <img
-            src={NoDiaryBear}
-            alt="다이어리 없을 때 곰돌이 그림"
-            onClick={() => {
-              setIsModal(true);
-            }}
-          />
-          <span>여기를 눌러 첫 다이어리를 만들어보세요</span>
+          <CreateDiaryModal>
+            <div className="addDiary">
+              <img src={NoDiaryBear} alt="다이어리 없을 때 곰돌이 그림" />
+              <span>여기를 눌러 첫 다이어리를 만들어보세요</span>
+            </div>
+          </CreateDiaryModal>
         </StWrapper>
       </StContainer>
-      {isModal && <CreateDiaryModal onClose={setIsModal}></CreateDiaryModal>}
     </>
   );
 };
@@ -34,10 +30,10 @@ const StContainer = styled.div`
 `;
 
 const StWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  ${flex("", "", "column")}
+  .addDiary {
+    ${flex("", "", "column")}
+  }
   img {
     width: 10.5rem;
     height: 11.5rem;
