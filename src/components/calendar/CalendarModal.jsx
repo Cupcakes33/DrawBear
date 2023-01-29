cd6e7bf7a417c436132c20f584a96b1c7a7c0278
jhchoi1182 <jhchoi1182@gmail.com>
2023-01-29 23:30:43 +0900

    ✨ ADD: 달 선택 박스 추가

--- src/components/calendar/CalendarModal.jsx
+++ src/components/calendar/CalendarModal.jsx
@@ -2,9 +2,11 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useMemo } from "react";
 import { useEffect } from "react";
 import { useCallback, useState } from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { diaryApi } from "../../apis/axios";
 import { Modal } from "../common/modal/ReactModal";
+import { GrPrevious, GrNext } from "react-icons/gr";
+import { FiChevronDown } from "react-icons/fi";
 
 const CalendarModal = ({ children }) => {
   const today = {
@@ -15,12 +17,20 @@ const CalendarModal = ({ children }) => {
   const [selectedYear, setSelectedYear] = useState(today.year);
   const [selectedMonth, setSelectedMonth] = useState(today.month);
   const [selectedDate, setSelectedDate] = useState("");
+  const [showMonth, setShowMonth] = useState(false);
 
   const { data = [], isLoading, isError } = useQuery(["holiday", selectedYear], () => diaryApi.holiday(selectedYear));
   const queryClient = useQueryClient();
 
   const holiday = data?.map((v) => v.locdate);
 
+  const month = useCallback(() => {
+    let monthArr = [];
+    for (let i = 1; i < 13; i++) {
+      monthArr.push(i);
+    }
+    return monthArr;
+  }, []);
   const week = useMemo(() => {
     return ["일", "월", "화", "수", "목", "금", "토"];
   }, []);
@@ -52,9 +62,6 @@ const CalendarModal = ({ children }) => {
     ));
   }, [week]);
 
-  // 주호님 방식에 맞춰서 둘 중 하나로 로직 수정
-  // 1. setquerydata를 이용해 캐시를 변경할 경우 -> 복사본 만들어서 복사본을 달력 표시로 쓰고 원본을 수정해 렌더링 변화주기
-  // 2. 캐시 건드리지 않고 임의의 변수 or 상수를 만들어서 렌더링에 사용하실 경우 -> 만들어 놓은 것에 덮어 씌워 렌더링 변화주기
   const postsMonthFilterFn = useCallback(() => {
     const postsYear = queryClient
       ?.getQueryData(["Allposts_copy"])
@@ -94,7 +101,7 @@ const CalendarModal = ({ children }) => {
             <button
               key={i}
               className={postedDayCompareFn(i) ? `${dayColor(i)} postedDay` : dayColor(i)}
-              onClick={() => setSelectedDate(i)}
+              onClick={() => selectedDatePostSearch(i)}
             >
               {i}
             </button>
@@ -107,11 +114,17 @@ const CalendarModal = ({ children }) => {
     return dayArr;
   }, [selectedYear, selectedMonth, holiday]);
 
-  const selectedDatePostSearch = () => {
+  const selectedDatePostSearch = (i) => {
+    setSelectedDate(i);
     const selectedDayPost = postsMonthFilterFn().filter(
       (post) => +post.createdAt.split("-")[2].split("T")[0] === selectedDate
     );
     queryClient.setQueryData(["Allposts"], selectedDayPost);
+  };
+
+  const monthController = (month) => {
+    setSelectedMonth(month);
+    setShowMonth(false);
   };
 
   useEffect(() => {
@@ -132,18 +145,40 @@ const CalendarModal = ({ children }) => {
                 <h2>서버 연결 실패</h2>
               ) : (
                 <>
-                  <StHeader>
-                    <h3>{`${selectedYear}년 ${selectedMonth}월`}</h3>
-                    <div className="buttons">
-                      <div>
-                        <button onClick={selectedDatePostSearch}>조회</button>
-                        <button onClick={() => prevMonth()}>이전 달</button>
-                        <button onClick={() => nextMonth()}>다음 달</button>
-                      </div>
+                  <CalendarHeaderBox>
+                    <div className="shown-date">
+                      <h3>{`${selectedYear}년`}</h3>
+                      {showMonth ? null : <h3>{`${selectedMonth}월`}</h3>}
+                      <FiChevronDown className="date-show-arrow" onClick={() => setShowMonth(true)} />
                     </div>
-                  </StHeader>
-                  <StWeek>{returnWeek()}</StWeek>
-                  <StDate>{returnDay()}</StDate>
+
+                    <div className="buttons">
+                      <button onClick={() => prevMonth()}>
+                        <GrPrevious />
+                      </button>
+                      <button onClick={() => nextMonth()}>
+                        <GrNext />
+                      </button>
+                    </div>
+                  </CalendarHeaderBox>
+                  {showMonth ? (
+                    <MonthBox>
+                      {month().map((month) => {
+                        return (
+                          <button
+                            key={month}
+                            className={month <= today.month ? "past" : "future"}
+                            onClick={() => monthController(month)}
+                          >{`${month}월`}</button>
+                        );
+                      })}
+                    </MonthBox>
+                  ) : (
+                    <>
+                      <WeekBox>{returnWeek()}</WeekBox>
+                      <DateBox>{returnDay()}</DateBox>
+                    </>
+                  )}
                 </>
               )}
             </CalendarContainer>
@@ -158,26 +193,67 @@ export default CalendarModal;
 
 const CalendarContainer = styled.section`
   width: 36rem;
-  height: 40rem;
+  height: 41rem;
   padding: 2rem 2rem;
   border: 1px solid black;
+  border-top: none;
   border-radius: 20px 20px 0px 0px;
   background-color: white;
 `;
 
-const StHeader = styled.div`
+const CalendarHeaderBox = styled.div`
   display: flex;
   justify-content: space-between;
-  color: #242424
+  color: #242424;
   margin-bottom: 2rem;
+  .shown-date {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    cursor: pointer;
+  }
+  .date-show-arrow {
+    color: #3cc7a6;
+    font-size: 2.5rem;
+  }
   .buttons {
     display: flex;
     align-items: center;
-    gap: 1rem;
+    gap: 2rem;
+  }
+  button {
+    border: none;
+    background-color: inherit;
+    cursor: pointer;
+    font-size: 2rem;
   }
 `;
 
-const StWeek = styled.div`
+const MonthBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 0.75rem;
+  button {
+    width: 10.1rem;
+    height: 6rem;
+    border: none;
+    border-radius: 9px;
+    font-size: 1.7rem;
+    margin-bottom: 1rem;
+    cursor: pointer;
+  }
+  .past {
+    background: #3cc7a6;
+    color: white;
+  }
+  .future {
+    background-color: #e8e8e8;
+    color: #b3b3b3;
+  }
+`;
+
+const WeekBox = styled.div`
   display: flex;
   .weekday {
     width: calc(36rem / 7);
@@ -192,7 +268,7 @@ const StWeek = styled.div`
   }
 `;
 
-const StDate = styled.div`
+const DateBox = styled.div`
   margin-top: 2rem;
   button {
     float: left;
