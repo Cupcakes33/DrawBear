015e34a28776aac261938dd95979a14f3fd0d440
jhchoi1182 <jhchoi1182@gmail.com>
2023-01-31 18:10:48 +0900

    ✨ ADD: 유효성 경고 문구 컴포넌트화

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,4 +1,4 @@
-import { css } from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Input = () => {
   return css`
@@ -8,7 +8,6 @@ export const Input = () => {
       border: none;
       border-radius: 10px;
       padding: 1rem;
-      outline: none;
       transition: box-shadow 0.4s;
       ::placeholder {
         color: #dedede;
@@ -17,10 +16,24 @@ export const Input = () => {
     .pass:focus {
       border: 1px solid #3cc7a5;
       box-shadow: 0 0 5px #3cc7a5;
+      outline: none;
     }
     .fail:focus {
       border: 1px solid #ff5656;
       box-shadow: 0 0 5px #ff5656;
+      outline: none;
     }
   `;
 };
+
+export const WorningWord = ({ children, color }) => {
+  return <ValidationText color={color ? "#ff5656" : "transparent"}>{children}</ValidationText>;
+};
+
+const ValidationText = styled.small`
+  color: ${({ color }) => color};
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`;
