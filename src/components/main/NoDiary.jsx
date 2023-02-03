3f6b0e5c1398eaf7335a8722e08089b57720b382
jhchoi1182 <jhchoi1182@gmail.com>
2023-02-04 01:16:01 +0900

    ✨ FIX: 북마크 관련 디자인 변경사항 반영

--- src/components/main/NoDiary.jsx
+++ src/components/main/NoDiary.jsx
@@ -2,19 +2,37 @@ import styled from "styled-components";
 import CreateDiaryModal from "./CreateDiaryModal";
 import NoDiaryBear from "../../assets/images/noDiaryBear.webp";
 import { flex } from "../../UI/common";
+import { useSelector } from "react-redux";
+import useDispatchHook from "../../hooks/useDispatchHook";
+import Diary from "./Diary/Diary";
 
 const NoDiary = () => {
+  const { icon } = useSelector((state) => state.diarySlice.diaryTypes);
+  const { changeDiaryView } = useDispatchHook();
+
   return (
     <>
       <StContainer>
         <StWrapper>
-          <h3>아직 다이어리가 없어요</h3>
-          <CreateDiaryModal>
-            <div className="addDiary">
-              <img src={NoDiaryBear} alt="다이어리 없을 때 곰돌이 그림" />
-              <span>여기를 눌러 첫 다이어리를 만들어보세요</span>
-            </div>
-          </CreateDiaryModal>
+          {icon === "bookmark" ? (
+            <>
+              <h3>즐겨찾기 한 다이어리가 없어요</h3>
+              <div className="addDiary">
+                <Diary size="bookmark" />
+                <span>책갈피 아이콘을 눌러 즐겨찾기 해보세요!</span>
+              </div>
+            </>
+          ) : (
+            <>
+              <h3>아직 다이어리가 없어요</h3>
+              <CreateDiaryModal>
+                <div className="addDiary">
+                  <img src={NoDiaryBear} alt="다이어리 없을 때 곰돌이 그림" />
+                  <span>여기를 눌러 첫 다이어리를 만들어보세요</span>
+                </div>
+              </CreateDiaryModal>
+            </>
+          )}
         </StWrapper>
       </StContainer>
     </>
