f34c8d755067595858dfe87f51b2ef400205fa18
jhchoi1182 <jhchoi1182@gmail.com>
2023-02-07 21:59:30 +0900

    ♻️ REFACTOR: 코드 스플리팅 작업 완료Resolves: #90

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -1,34 +1,29 @@
 import React from "react";
 import styled from "styled-components";
-import { flex } from "../../UI/common";
 import loadingBear from "../../assets/images/loadingBear.webp";
+import { flex } from "../../UI/common";
+import Layout from "./Layout";
 
 const Loading = ({ children }) => {
   return (
+    <Layout>
       <LoadingContainer>
-        <div className="loading-box">
-          <img src={loadingBear} alt="로딩 곰돌이" />
-          <h2>{children ? children : "로딩 중..."}</h2>
-        </div>
+        <img src={loadingBear} alt="로딩 곰돌이" />
+        <h2>{children ? children : "로딩 중..."}</h2>
       </LoadingContainer>
+    </Layout>
   );
 };
 
 export default Loading;
 
 const LoadingContainer = styled.div`
-  width: 100%;
-  height: 99vh;
-  ${flex}
-  .loading-box {
-    text-align: center;
-    img {
-      margin-right: 1.5rem;
-    }
-    h2 {
-      display: flex;
-      justify-content: center;
-      margin-top: 1rem;
-    }
+  height: 100vh;
+  ${flex("", "", "column")}
+  img {
+    margin-right: 1.5rem;
+  }
+  h2 {
+    margin-top: 1rem;
   }
 `;
